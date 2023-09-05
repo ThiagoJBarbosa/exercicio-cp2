e249356ae9ac5160900058d86fede6a60745d852
ThiagoJBarbosa <thiagojesusbs@gmail.com>
2023-09-05 10:57:12 -0300

    Importando os elementos do router e as páginas no main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {createBrowserRouter, Routerprovider } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Error from './routes/EditarProdutos/index.jsx'
+import EditarProduto from './routes/EditarProdutos/index.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
