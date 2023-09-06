c04cbba59b2cef92fead6f98a4bb3cf90abe2af0
ThiagoJBarbosa <thiagojesusbs@gmail.com>
2023-09-06 08:06:24 -0300

    Arrumando o RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import {createBrowserRouter, Routerprovider } from 'react-router-dom'
+import {createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
 import Error from './routes/Error/index.jsx'
-import EditarProduto from './routes/EditarProduto/index.jsx'
+import EditarProdutos from './routes/EditarProdutos/index.jsx'
+
 
 const router = createBrowserRouter([
   {
@@ -25,7 +26,7 @@ const router = createBrowserRouter([
       },
       {
         path:"/produto/editar/:id",
-        element: <EditarProduto/>
+        element: <EditarProdutos/>
       }
     ]
   }
@@ -33,6 +34,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Routerprovider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
